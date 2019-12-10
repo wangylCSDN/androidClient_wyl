10c2c76571789dd3e9b5da21c9cb279edbe943dd
wyl <1191469070@qq.com>
2019-12-10 10:16:42 +0800

    12.10

--- app/src/main/java/com/example/wqter/androidclient_mypart/DeviceConfirmActivity.java
+++ app/src/main/java/com/example/wqter/androidclient_mypart/DeviceConfirmActivity.java
@@ -1,18 +1,27 @@
 package com.example.wqter.androidclient_mypart;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Created by wqter on 2019/12/6.
  */
 
-public class DeviceConfirmActivity extends AppCompatActivity {
+public class DeviceConfirmActivity extends AppCompatActivity implements View.OnClickListener{
+    private Spinner com0_sp,com0_sp1;
+    private Button sta_btn,confirm_btn,com_btn,com_btn_change;
+    private EditText device_num;
+    private AlertDialog.Builder com0_bd;
+    private AlertDialog com0_ad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature( Window.FEATURE_NO_TITLE);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deviceconfirm);
         if (getSupportActionBar() != null) {
@@ -22,6 +31,12 @@ public class DeviceConfirmActivity extends AppCompatActivity {
 
     }
     private void initView(){
+        com0_sp=(Spinner)findViewById(R.id.com0_sp);
+        com0_sp1=(Spinner)findViewById(R.id.com0_sp1);
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
